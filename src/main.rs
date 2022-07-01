9ae73294d8309da6d074f52f59d860c8f5c4625e
Simonas Narbutas <snarbutas@bol.com>
2022-07-01 12:05:42 +0200

    add clap dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// An utility to do magic with your photos
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// A path to search photos in
+    #[clap(short, long, value_parser)]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("path provided: {}!", args.path);
 }
