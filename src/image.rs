328c4060853db7436c4acd6eb25bd67130610ea4
Quido Meijer <qmeijer@bol.com>
2022-07-01 16:59:40 +0200

    Adds comment and removes a lot of lines
    also compact it a bit more

--- src/image.rs
+++ src/image.rs
@@ -1,18 +1,10 @@
 use std::path::Path;
 
+/// image_or_video determines whether a given path is an image or video file
 pub fn image_or_video<P: AsRef<Path>>(filename: &P) -> bool {
     match infer::get_from_path(filename) {
-        Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Image => {
-            true
-        }
-        Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Video => {
-            true
-        }
-        Err(e) => {
-            eprintln!("Looks like something went wrong 😔");
-            eprintln!("{}", e);
-            false
-        }
+        Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Image => { true }
+        Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Video => { true }
         _ => { false }
     }
 }
