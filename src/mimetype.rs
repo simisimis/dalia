fd895fb626f660ba79475a3a5e37ad36fd50b3af
Simonas Narbutas <simonas@narbuto.lt>
2022-10-16 21:52:31 +0200

    skip check if directories are images/videos

--- src/mimetype.rs
+++ src/mimetype.rs
@@ -1,11 +1,15 @@
-use std::{io, path::Path};
+use std::{fs, io, path::Path};
 
 /// image_or_video determines whether a given path is an image or video file
 pub fn image_or_video<P: AsRef<Path>>(filename: &P) -> Result<bool, io::Error> {
-    match infer::get_from_path(filename) {
-        Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Image => Ok(true),
-        Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Video => Ok(true),
-        Ok(_) => Ok(false),
-        Err(e) => Err(e),
+    if fs::metadata(filename)?.is_dir() {
+        Ok(false)
+    } else {
+        match infer::get_from_path(filename) {
+            Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Image => Ok(true),
+            Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Video => Ok(true),
+            Ok(_) => Ok(false),
+            Err(e) => Err(e),
+        }
     }
 }
