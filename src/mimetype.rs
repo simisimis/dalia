ce20a89213502265b2931da7f8272779f532e653
Simonas Narbutas <simonas@narbuto.lt>
2022-10-16 22:16:03 +0200

    rename filename to path

--- src/mimetype.rs
+++ src/mimetype.rs
@@ -1,11 +1,11 @@
 use std::{fs, io, path::Path};
 
 /// image_or_video determines whether a given path is an image or video file
-pub fn image_or_video<P: AsRef<Path>>(filename: &P) -> Result<bool, io::Error> {
-    if fs::metadata(filename)?.is_dir() {
+pub fn image_or_video<P: AsRef<Path>>(path: &P) -> Result<bool, io::Error> {
+    if fs::metadata(path)?.is_dir() {
         Ok(false)
     } else {
-        match infer::get_from_path(filename) {
+        match infer::get_from_path(path) {
             Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Image => Ok(true),
             Ok(Some(info)) if info.matcher_type() == infer::MatcherType::Video => Ok(true),
             Ok(_) => Ok(false),
